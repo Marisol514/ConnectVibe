387f0cff874c429640fb04d2c55f06fd415da256
Marisol514 <145890422+Marisol514@users.noreply.github.com>
2024-05-08 20:39:08 -0700

    updated the users js to correct the connectity issue

--- models/User.js
+++ models/User.js
@@ -32,7 +32,12 @@ userSchema.index({ username: 1 });  // Index on username for quick look-up
 userSchema.index({ email: 1 });     // Index on email for quick look-up
 
 userSchema.virtual('friendCount').get(function() {
-  return this.friends.length;
+  // Check if this.friends exists and is an array
+  if (this.friends && Array.isArray(this.friends)) {
+    return this.friends.length;
+  } else {
+    return 0; // Return 0 if this.friends is not defined or not an array
+  }
 });
 
 const User = mongoose.model('User', userSchema);
